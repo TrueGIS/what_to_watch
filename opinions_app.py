f4ec07601a26af80768c897b576ee9b40fc0ad1d
Anton Makarov <makaranto@yandex.ru>
2026-09-21 22:39:11 +0800

    add model and query

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,43 @@
+# what_to_watch/opinions_app.py
+
+from datetime import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from random import randrange
 
 app = Flask(__name__)
 
+# Подключить БД SQLite.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+
+class Opinion(db.Model):
+    # ID — целое число, первичный ключ.
+    id = db.Column(db.Integer, primary_key=True)
+    # Название фильма — строка длиной 128 символов, не может быть пустым.
+    title = db.Column(db.String(128), nullable=False)
+    # Мнение о фильме — большая строка, не может быть пустым, 
+    # должно быть уникальным.
+    text = db.Column(db.Text, unique=True, nullable=False)
+    # Ссылка на сторонний источник — строка длиной 256 символов.
+    source = db.Column(db.String(256))
+    # Дата и время — текущее время, 
+    # по этому столбцу база данных будет проиндексирована.
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    # Определить количество мнений в базе данных.
+    quantity = Opinion.query.count()
+    # Если мнений нет...
+    if not quantity:
+        # ...то вернуть сообщение:
+        return 'В базе данных мнений о фильмах нет.'
+    # Иначе выбрать случайное число в диапазоне от 0 до quantity...
+    offset_value = randrange(quantity)
+    # ...и определить случайный объект.
+    opinion = Opinion.query.offset(offset_value).first()
+    return opinion.text
 
 if __name__ == '__main__':
     app.run()
